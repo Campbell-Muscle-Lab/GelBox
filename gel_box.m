7091ee2242f5c8bfd7630928dff444d5ae0cbe5a
utkugulbulak <98066302+utkugulbulak@users.noreply.github.com>
2023-05-02 19:25:25 -0400

    Update gel_box.m

--- gel_box.m
+++ gel_box.m
@@ -2,6 +2,9 @@ function gelbox
 
 version_string = 'GelBox Version 2.0';
 
-addpath('box','interface','update','utilities');
+addpath('../MATLAB_Utilities');
+addpath('box','interface','update');
+
+
 
 gui = create_interface(version_string);
