06a51626999f5b6229e380c7f3afd7768366d8f1
utkugulbulak <98066302+utkugulbulak@users.noreply.github.com>
2023-05-02 19:20:24 -0400

    tweaking gel_box.m

--- gel_box.m
+++ gel_box.m
@@ -2,6 +2,6 @@ function gelbox
 
 version_string = 'GelBox Version 2.0';
 
-addpath('box','interface','update');
+addpath('box','interface','update','utilities');
 
 gui = create_interface(version_string);
